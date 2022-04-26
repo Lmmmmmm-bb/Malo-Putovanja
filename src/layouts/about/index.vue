08f7c2db7c93ab51b36a55142f76fb8172cc1030
_lmmmmmm <931420654@qq.com>
2022-04-26 20:52:11 +0800

    chore: example page

--- src/layouts/about/index.vue
+++ src/layouts/about/index.vue
@@ -1,3 +1,13 @@
+<script setup lang="ts">
+import { NButton } from 'naive-ui';
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+</script>
+
 <template>
-  <div>about</div>
+  <div>
+    <h2 class="text-3xl font-bold">About</h2>
+    <n-button type="primary" secondary @click="router.back()">To Home</n-button>
+  </div>
 </template>
--- src/layouts/home/index.vue
+++ src/layouts/home/index.vue
@@ -1,3 +1,16 @@
+<script setup lang="ts">
+import { NButton } from 'naive-ui';
+import { useRouter } from 'vue-router';
+import styles from './index.module.scss';
+
+const router = useRouter();
+</script>
+
 <template>
-  <div>home</div>
+  <div :class="styles.wrapper">
+    <h2 class="text-3xl font-bold">Home</h2>
+    <n-button type="primary" @click="router.push({ name: 'about' })">
+      To About
+    </n-button>
+  </div>
 </template>
