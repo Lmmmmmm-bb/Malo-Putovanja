1c6a901fb45118dd4962f0a8d3a313ed4d0fabbe
_lmmmmmm <931420654@qq.com>
2022-04-27 17:39:17 +0800

    feat(components): menu navigator

--- src/layouts/home/index.vue
+++ src/layouts/home/index.vue
@@ -1,25 +1,10 @@
 <script setup lang="ts">
-import { NButton } from 'naive-ui';
-import { useRouter } from 'vue-router';
-import styles from './index.module.scss';
-import { useCounterStore } from '@/store';
-
-const router = useRouter();
-const countStore = useCounterStore();
+import MenuNavigator from './components/menu-navigator.vue';
 </script>
 
 <template>
-  <div :class="styles.wrapper">
+  <div class="h-full w-full flex flex-col">
     <h2 class="text-3xl font-bold">Home</h2>
-    <p class="text-2xl">
-      count: {{ countStore.count }}, double: {{ countStore.double }}
-    </p>
-    <div>
-      <n-button type="primary" @click="countStore.count++">+</n-button>
-      <n-button type="primary" @click="countStore.count--">-</n-button>
-    </div>
-    <n-button type="primary" @click="router.push({ name: 'about' })">
-      To About
-    </n-button>
+    <menu-navigator class="absolute bottom-0" />
   </div>
 </template>
